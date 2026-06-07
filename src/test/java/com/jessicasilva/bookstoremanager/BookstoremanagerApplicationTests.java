12b341be10553c8beb3e74dbbcd20d18c95af6bf
Vitoria8no <jessicadasilvaesilva8@gmail.com>
2026-06-07 00:14:08 -0300

    first commit:project structure created

---
+++ src/test/java/com/jessicasilva/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.jessicasilva.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	
+
+}
